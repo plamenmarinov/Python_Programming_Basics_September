6363bb784f19058169e8236a6c7cf651d8300f16
plamenmarinov <117172634+plamenmarinov@users.noreply.github.com>
2023-05-29 12:52:05 +0300

    Delete 3. Първи стъпки в програмирането directory

--- 3. Първи стъпки в програмирането/Projects_Creation.py
+++
@@ -1,6 +0,0 @@
-architect_name = input()
-number_of_projects = int(input())
-time_to_complete_all_projects = number_of_projects * 3
-
-print(f'The architect {architect_name} will need {time_to_complete_all_projects} hours to complete {number_of_projects}'
-      f' project/s.')
